dc13459214ff95b3ddc06d9463ebeca3674829a2
mfantonidas <mfantonidas@gmail.com>
2010-06-05 11:44:25 +0800

    another

---
+++ SerialComm.c
@@ -0,0 +1,23 @@
+#include "SerialComm.h"
+#include <stdio.h>
+
+static struct SensorNode *sensors;
+
+static void init_sensor()
+{
+  struct SensorNode *sn;
+  int counter = 0;
+  int i = 0;
+  
+  /*
+   *  Codes to read 
+   *
+   * */
+}
+
+static void read_sensor()
+{
+  /*
+   *  
+   * */
+}
